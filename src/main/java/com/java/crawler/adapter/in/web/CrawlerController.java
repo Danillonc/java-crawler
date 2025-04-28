d2f428a2b0e827803534f35885bf54845ac04e25
Danillo <danillonc@live.com>
2025-04-27 22:05:25 -0300

    Refactoring DTOs to use java record class and implementing controller layer

--- src/main/java/com/java/crawler/adapter/in/web/CrawlerController.java
+++ src/main/java/com/java/crawler/adapter/in/web/CrawlerController.java
@@ -1,4 +1,40 @@
 package com.java.crawler.adapter.in.web;
 
+import com.java.crawler.adapter.in.web.dto.CrawlerRequestDTO;
+import com.java.crawler.adapter.in.web.dto.CrawlerResponseDTO;
+import com.java.crawler.application.port.in.GetCrawlerResultQuery;
+import com.java.crawler.application.port.in.StartCrawlerUseCase;
+import com.java.crawler.domain.model.CrawlerTask;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class CrawlerController {
+
+    private final StartCrawlerUseCase startCrawlerUseCase;
+    private final GetCrawlerResultQuery getCrawlerResultQuery;
+
+    public CrawlerController(StartCrawlerUseCase startCrawlerUseCase, GetCrawlerResultQuery getCrawlerResultQuery) {
+        this.startCrawlerUseCase = startCrawlerUseCase;
+        this.getCrawlerResultQuery = getCrawlerResultQuery;
+    }
+
+    @PostMapping("/crawler")
+    public ResponseEntity<Map<String, String>> startCrawler(@Valid @RequestBody CrawlerRequestDTO request) {
+        String id = this.startCrawlerUseCase.startSearch(request.keyword());
+        return ResponseEntity.ok(Map.of("id", id));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CrawlerResponseDTO> get(@PathVariable String id) {
+        CrawlerTask task = getCrawlerResultQuery.getCrawlerTask(id);
+        return ResponseEntity.ok(new CrawlerResponseDTO(task));
+    }
+
+
+
 }
--- src/main/java/com/java/crawler/adapter/in/web/dto/CrawlerResponseDTO.java
+++ src/main/java/com/java/crawler/adapter/in/web/dto/CrawlerResponseDTO.java
@@ -1,9 +1,15 @@
 package com.java.crawler.adapter.in.web.dto;
 
+import com.java.crawler.domain.model.CrawlerTask;
+
 import java.util.Set;
 
-public class CrawlerResponseDTO {
-    private String id;
-    private String status;
-    private Set<String> urls;
+public record CrawlerResponseDTO (
+    String id,
+    String status,
+    Set<String> urls) {
+
+    public CrawlerResponseDTO(CrawlerTask task) {
+        this(task.getId(), task.getStatus(), task.getFoundUrls());
+    }
 }
